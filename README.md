9e51e5399f3be205bdf11c446784897c6a55a5c8
Luisa Friebel <luisa.friebel@stud.th-deg.de>
2024-12-14 20:57:02 +0100

    14.12.2024:
    fixed "start game" button to activate the timeline "000triggers"
    dialouge for stat introduction in "002first_day"
    added scene calendartest with containers for a weekly planning

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This repo saves my efforts for the class game programming of semester 7 of my artificial intelligence bachelor.
 The game engine is Godot!
+
+
+progress:
+* 13.12.2024: initialized git for this project 
+* 14.12.2024: 
+    fixed "start game" button to activate the timeline "000triggers"
+    dialouge for stat introduction in "002first_day"
+    added scene calendartest with containers for a weekly planning 
--- game/button.gd
+++ game/button.gd
@@ -1,15 +1,31 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	Dialogic.signal_event.connect(DialogicSignal)
+	pass
+	# Dialogic.start("000triggers")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	pass
 
 func _on_pressed():
-	Dialogic.start_timeline("timeline")
+	Dialogic.start("000triggers")
 	pass
+# Called when the node enters the scene tree for the first time.
+
+
+#func _on_pressed():
+	#Dialogic.timeline_ended.connect(ended)
+	#Dialogic.start("002first_day")
+
+func ended ():
+	Dialogic.timeline_ended.disconnect(ended)
+	print("ended")
+	
+func DialogicSignal(argument:String):
+	if argument =="test":
+		print("Signal Received")
